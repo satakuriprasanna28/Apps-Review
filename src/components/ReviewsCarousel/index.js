0a9021e211a0f81f91a8eb360871a7b99d285baa
satakuriprasanna28 <satakuriprasanna@gmail.com>
2023-06-27 05:31:48 +0000

    rendererror

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,78 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {activeReviewIndex: 0}
+
+  getReview = reviewsList => {
+    const {activeReviewIndex} = this.state
+    const {imgUrl, username, companyName, description} = reviewsList[
+      activeReviewIndex
+    ]
+
+    return (
+      <div className="review-container">
+        <img src={imgUrl} alt={username} />
+        <p className="username">{username}</p>
+        <p className="company">{companyName}</p>
+        <p className="description">{description}</p>
+      </div>
+    )
+  }
+
+  onLeft = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  onRight = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+
+    return (
+      <div className="app-container">
+        <button
+          className="arrow-button"
+          type="button"
+          onClick={this.onLeft}
+          data-testId="leftArrow"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+            alt="left arrow"
+          />
+        </button>
+        {this.getReview(reviewsList)}
+        <button
+          type="button"
+          className="arrow-button"
+          onClick={this.onClickRightArrow}
+          data-testId="rightArrow"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+            alt="right arrow"
+          />
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
